96d0f6978063105cb8de6015a8e14ba885255536
wnusiek <120226590+wnusiek@users.noreply.github.com>
2023-02-05 13:43:16 +0100

    implemented min  function

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,19 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
     }
+
+
+    public static void min (Integer[] numbers){
+        Integer min = Integer.MAX_VALUE;
+
+        for (Integer num : numbers){
+            if (num < min){
+                min = num;
+            }
+        }
+        return min;
+    }
+
 }
